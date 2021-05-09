103f5deb93c72eba38fdaa6b5fa59a5dfb21d0a6
叶奕 <simon.ye@hongkongmedsourcing.com>
2021-05-09 11:04:53 +0800

    订单列表完成

--- lib/manage/order/order_page.dart
+++ lib/manage/order/order_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:logistics/comm/color.dart';
 import 'package:logistics/comm/logger.dart';
 import 'package:logistics/manage/order/order_nao.dart';
+import 'package:toast/toast.dart';
 
 class OrderPage extends StatefulWidget {
   @override
@@ -35,6 +37,8 @@ class _OrderListPageState extends State<_OrderListPage> {
   final TextEditingController textEditingController = TextEditingController();
   final OrderNao orderNao = OrderNao();
   List<OrderDTO> orders = [];
+  OrderDTO? searchedOrder;
+  int? selectedItem = 0;
 
   @override
   void initState() {
@@ -71,36 +75,56 @@ class _OrderListPageState extends State<_OrderListPage> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Flexible(child: SizedBox(
-          width: 300,
-          height: 32,
-          child: TextField(
-            onChanged: (text) {},
-            controller: textEditingController,
-            style: TextStyle(fontSize: 14),
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.white,
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Theme.of(context).dividerColor),
-                borderRadius: BorderRadius.all(Radius.circular(12)),
+        Flexible(
+          child: SizedBox(
+            width: 300,
+            height: 32,
+            child: TextField(
+              onChanged: (text) {},
+              controller: textEditingController,
+              style: TextStyle(fontSize: 14),
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp("^[A-Za-z0-9]+\$")),
+              ],
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Theme.of(context).dividerColor),
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Theme.of(context).dividerColor),
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+                hintText: "输入订单号搜索",
               ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Theme.of(context).dividerColor),
-                borderRadius: BorderRadius.all(Radius.circular(12)),
-              ),
-              contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 16),
-              hintText: "输入订单号搜索",
             ),
           ),
-        ),),
+        ),
         SizedBox(width: 6),
         ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            searchAndShowOrder(textEditingController.text);
+          },
           child: Text("搜索"),
         ),
       ],
     );
+  }
+
+  void searchAndShowOrder(String orderNo) async {
+    if (orderNo == "") return;
+    try {
+      searchedOrder = await orderNao.searchOrder(orderNo);
+      selectedItem = null;
+      setState(() {});
+    } catch (e) {
+      logger.w(e);
+      Toast.show("请检查订单号是否正确", context, duration: 3);
+    }
   }
 
   Widget orderListView() {
@@ -112,32 +136,62 @@ class _OrderListPageState extends State<_OrderListPage> {
             child: orderItemWidget("运单号", "转单号", "下单时间", "目的地"),
           ),
           SizedBox(height: 6),
+          searchedOrder == null
+              ? SizedBox()
+              : Container(
+                  color: selectedItem == null
+                      ? Colors.grey[200]
+                      : Colors.transparent,
+                  margin: EdgeInsets.only(bottom: 6),
+                  child: GestureDetector(
+                    onTap: () {
+                      selectedItem = null;
+                      setState(() {});
+                    },
+                    child: orderItemWidget(
+                        searchedOrder!.no,
+                        searchedOrder!.delegateOrders.isEmpty
+                            ? ""
+                            : searchedOrder!.delegateOrders[0].no,
+                        searchedOrder!.time,
+                        searchedOrder!.to.address),
+                  ),
+                ),
           Flexible(
             child: ListView.builder(
               itemBuilder: (context, index) {
                 final order = orders[index];
-                return orderItemWidget(
-                    order.no,
-                    order.delegateOrders.isEmpty
-                        ? ""
-                        : order.delegateOrders[0].no,
-                    order.time,
-                    order.to.address);
+                return GestureDetector(
+                  onTap: () {
+                    selectedItem = index;
+                    setState(() {});
+                  },
+                  child: orderItemWidget(
+                      order.no,
+                      order.delegateOrders.isEmpty
+                          ? ""
+                          : order.delegateOrders[0].no,
+                      order.time,
+                      order.to.address,
+                      selectedItem == index),
+                );
               },
               itemCount: orders.length,
               shrinkWrap: true,
             ),
           ),
           SizedBox(height: 10),
-          GestureDetector(
-            onTap: () {},
-            child: Container(
-              height: 30,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                border: Border.all(color: Theme.of(context).dividerColor),
-              ),
-              child: Icon(Icons.add),
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              border: Border.all(color: Theme.of(context).dividerColor),
+            ),
+            child: IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                selectedItem = null;
+                setState(() {});
+              },
             ),
           ),
           SizedBox(height: 32),
@@ -147,14 +201,18 @@ class _OrderListPageState extends State<_OrderListPage> {
   }
 
   Widget orderItemWidget(String orderNo, String delegateOrderNo,
-      String orderTime, String destination) {
-    return Row(
-      children: [
-        cellWidget(1, orderNo),
-        cellWidget(1, delegateOrderNo),
-        cellWidget(1, orderTime),
-        cellWidget(1, destination),
-      ],
+      String orderTime, String destination,
+      [bool selected = false]) {
+    return Container(
+      color: selected ? Colors.grey[200] : Colors.transparent,
+      child: Row(
+        children: [
+          cellWidget(1, orderNo),
+          cellWidget(1, delegateOrderNo),
+          cellWidget(1, orderTime),
+          cellWidget(1, destination),
+        ],
+      ),
     );
   }
 
